745f9cdb2da7866ff0c96557b427f883bb7dfde8
alex-yates-redgate <alex.yates@red-gate.com>
2024-04-29 11:43:52 +0100

    Adding an rgclone version test

--- version.Tests.ps1
+++ version.Tests.ps1
@@ -1,10 +1,18 @@
-BeforeAll {
-    $flywayVersion = flyway version
-}
-
 Describe 'Checking the following tools are up to date' {
     It 'Flyway' {
+        $flywayVersion = flyway version
         If ($flywayVersion -like "*WARNING: This version of Flyway is out of date.*"){
+            $upToDate = $false
+        }
+        else {
+            $upToDate = $true
+        }
+        $upToDate | Should -BeTrue
+    }
+
+    It 'rgclone CLI' {
+        $rgcloneVersion = rgclone version
+        If ($rgcloneVersion -like "*WARNING - Your current version * of the rgclone CLI is outdated.*"){
             $upToDate = $false
         }
         else {
