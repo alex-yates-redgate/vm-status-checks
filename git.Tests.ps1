c79e8b26bbb4e52db99946cf6eac1c21a1c17b48
RyanSpr <162704784+RyanSpr@users.noreply.github.com>
2024-05-09 23:07:34 +0100

    Update git.Tests.ps1
    added version check to git.Tests.ps1

--- git.Tests.ps1
+++ git.Tests.ps1
@@ -1,5 +1,17 @@
 BeforeAll {
     $gitDirectory = "C:\git"
+
+    function Get-LatestGitHubCommitHash {
+        param (
+            [string]$GitHubRepository,
+            [string]$Branch
+        )
+    
+        $url = "https://api.github.com/repos/$GitHubRepository/commits/$Branch"
+        $response = Invoke-RestMethod -Uri $url
+    
+        return $response.sha
+    }
 }
 
 Describe 'Checking the following tools are up to date' {
@@ -19,6 +31,16 @@ Describe 'Checking the following tools are up to date' {
         $gitDirectoryExists | Should -BeTrue
     }
 
+    It "vm-status-checks repository should be latest version from Github" {
+        $gitDirectoryLatestVersion = $false
+        $latestCommitHash = Get-LatestGitHubCommitHash -GitHubRepository "alex-yates-redgate/vm-status-checks" -Branch 'main' #gets latest github commit hash
+        $currentCommitHash = (git -C "$gitDirectory\vm-status-checks" log -1).Split() | Where-Object { $_.Length -eq 40 } #gets local commit hash
+        If ($latestCommitHash -eq $currentCommitHash){
+            $gitDirectoryLatestVersion = $true
+        }
+        $gitDirectoryLatestVersion | Should -BeTrue
+    }
+
     It 'tdm-demos repository should be cloned to C:\git' {
         $gitDirectoryExists = $false
         If (Test-Path -Path "$gitDirectory\tdm-demos\.git"){
@@ -27,11 +49,31 @@ Describe 'Checking the following tools are up to date' {
         $gitDirectoryExists | Should -BeTrue
     }
 
+    It "tdm-demos repository should be latest version from Github" {
+        $gitDirectoryLatestVersion = $false
+        $latestCommitHash = Get-LatestGitHubCommitHash -GitHubRepository "alex-yates-redgate/tdm-demos" -Branch 'main' #gets latest github commit hash
+        $currentCommitHash = (git -C "$gitDirectory\tdm-demos" log -1).Split() | Where-Object { $_.Length -eq 40 } #gets local commit hash
+        If ($latestCommitHash -eq $currentCommitHash){
+            $gitDirectoryLatestVersion = $true
+        }
+        $gitDirectoryLatestVersion | Should -BeTrue
+    }
+
     It 'forkable-widget repository should be cloned to C:\git' {
         $gitDirectoryExists = $false
         If (Test-Path -Path "$gitDirectory\forkable-widget\.git"){
             $gitDirectoryExists = $true
         }
         $gitDirectoryExists | Should -BeTrue
+    }
+
+    It "forkable-widget repository should be latest version from Github" {
+        $gitDirectoryLatestVersion = $false
+        $latestCommitHash = Get-LatestGitHubCommitHash -GitHubRepository "alex-yates-redgate/forkable-widget" -Branch 'main' #gets latest github commit hash
+        $currentCommitHash = (git -C "$gitDirectory\forkable-widget" log -1).Split() | Where-Object { $_.Length -eq 40 } #gets local commit hash
+        If ($latestCommitHash -eq $currentCommitHash){
+            $gitDirectoryLatestVersion = $true
+        }
+        $gitDirectoryLatestVersion | Should -BeTrue
     }
 }
