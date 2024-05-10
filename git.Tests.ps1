374036a88ebddfca3c078a66fb67c2ba16b62485
RyanSpr <162704784+RyanSpr@users.noreply.github.com>
2024-05-10 14:25:50 +0100

    Implemented loops for repository names

--- git.Tests.ps1
+++ git.Tests.ps1
@@ -1,5 +1,6 @@
 BeforeAll {
     $gitDirectory = "C:\git"
+    $repoList = "vm-status-checks", "tdm-demos", "forkable-widget"
 
     function Get-LatestGitHubCommitHash {
         param (
@@ -14,66 +15,30 @@ BeforeAll {
     }
 }
 
-Describe 'Checking the following tools are up to date' {
-    It 'Git directory should exist at C:\git' {
-        $gitDirectoryExists = $false
-        If (Test-Path -Path $gitDirectory){
-            $gitDirectoryExists = $true
-        }
+Describe 'Checking the following directory exists' {
+    It 'C:\git' {
+        $gitDirectoryExists = Test-Path -Path $gitDirectory
         $gitDirectoryExists | Should -BeTrue
     }
+}
 
-    It 'vm-status-checks repository should be cloned to C:\git' {
-        $gitDirectoryExists = $false
-        If (Test-Path -Path "$gitDirectory\vm-status-checks"){
-            $gitDirectoryExists = $true
+Describe "Important GitHub Repositories" {
+    Context "<_>" -ForEach 'vm-status-checks','tdm-demos','forkable-widget' {
+        It 'should be cloned to C:\git' {
+            $repoPath = Join-Path -Path $gitDirectory -ChildPath $_
+            Test-Path -Path $repoPath | Should -BeTrue
         }
-        $gitDirectoryExists | Should -BeTrue
-    }
 
-    It "vm-status-checks repository should be latest version from Github" {
-        $gitDirectoryLatestVersion = $false
-        $latestCommitHash = Get-LatestGitHubCommitHash -GitHubRepository "alex-yates-redgate/vm-status-checks" -Branch 'main' #gets latest github commit hash
-        $currentCommitHash = (git -C "$gitDirectory\vm-status-checks" log -1).Split() | Where-Object { $_.Length -eq 40 } #gets local commit hash
-        If ($latestCommitHash -eq $currentCommitHash){
-            $gitDirectoryLatestVersion = $true
+        It "should be the latest version" {
+            $gitDirectoryLatestVersion = $false
+            $latestCommitHash = Get-LatestGitHubCommitHash -GitHubRepository "alex-yates-redgate/$_" -Branch 'main' #gets latest github commit hash
+            Write-Output $latestCommitHash
+            $currentCommitHash = (git -C "$gitDirectory\$_" log -1).Split() | Where-Object { $_.Length -eq 40 } #gets local commit hash
+            Write-Output $currentCommitHash
+            If ($latestCommitHash -eq $currentCommitHash){
+                $gitDirectoryLatestVersion = $true
+            }
+            $gitDirectoryLatestVersion | Should -BeTrue
         }
-        $gitDirectoryLatestVersion | Should -BeTrue
-    }
-
-    It 'tdm-demos repository should be cloned to C:\git' {
-        $gitDirectoryExists = $false
-        If (Test-Path -Path "$gitDirectory\tdm-demos\.git"){
-            $gitDirectoryExists = $true
-        }
-        $gitDirectoryExists | Should -BeTrue
-    }
-
-    It "tdm-demos repository should be latest version from Github" {
-        $gitDirectoryLatestVersion = $false
-        $latestCommitHash = Get-LatestGitHubCommitHash -GitHubRepository "alex-yates-redgate/tdm-demos" -Branch 'main' #gets latest github commit hash
-        $currentCommitHash = (git -C "$gitDirectory\tdm-demos" log -1).Split() | Where-Object { $_.Length -eq 40 } #gets local commit hash
-        If ($latestCommitHash -eq $currentCommitHash){
-            $gitDirectoryLatestVersion = $true
-        }
-        $gitDirectoryLatestVersion | Should -BeTrue
-    }
-
-    It 'forkable-widget repository should be cloned to C:\git' {
-        $gitDirectoryExists = $false
-        If (Test-Path -Path "$gitDirectory\forkable-widget\.git"){
-            $gitDirectoryExists = $true
-        }
-        $gitDirectoryExists | Should -BeTrue
-    }
-
-    It "forkable-widget repository should be latest version from Github" {
-        $gitDirectoryLatestVersion = $false
-        $latestCommitHash = Get-LatestGitHubCommitHash -GitHubRepository "alex-yates-redgate/forkable-widget" -Branch 'main' #gets latest github commit hash
-        $currentCommitHash = (git -C "$gitDirectory\forkable-widget" log -1).Split() | Where-Object { $_.Length -eq 40 } #gets local commit hash
-        If ($latestCommitHash -eq $currentCommitHash){
-            $gitDirectoryLatestVersion = $true
-        }
-        $gitDirectoryLatestVersion | Should -BeTrue
     }
 }
