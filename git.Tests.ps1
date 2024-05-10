13ddd0e5108fdc84131395aeab72c155688b9ca6
RyanSpr <162704784+RyanSpr@users.noreply.github.com>
2024-05-10 15:22:57 +0100

    Improved git tests. (Squashing and merging 3 commits from @RyanSpr.)
    * Update git.Tests.ps1
    
    added version check to git.Tests.ps1
    
    * Implemented loops for repository names
    
    * Improved code readability and conciseness

--- git.Tests.ps1
+++ git.Tests.ps1
@@ -1,37 +1,38 @@
 BeforeAll {
     $gitDirectory = "C:\git"
+    $repoList = "vm-status-checks", "tdm-demos", "forkable-widget"
+
+    function Get-LatestGitHubCommitHash {
+        param (
+            [string]$GitHubRepository,
+            [string]$Branch
+        )
+    
+        $url = "https://api.github.com/repos/$GitHubRepository/commits/$Branch"
+        $response = Invoke-RestMethod -Uri $url
+    
+        return $response.sha
+    }
 }
 
-Describe 'Checking the following tools are up to date' {
-    It 'Git directory should exist at C:\git' {
-        $gitDirectoryExists = $false
-        If (Test-Path -Path $gitDirectory){
-            $gitDirectoryExists = $true
-        }
+Describe 'Checking the following directory exists' {
+    It 'C:\git' {
+        $gitDirectoryExists = Test-Path -Path $gitDirectory
         $gitDirectoryExists | Should -BeTrue
     }
+}
 
-    It 'vm-status-checks repository should be cloned to C:\git' {
-        $gitDirectoryExists = $false
-        If (Test-Path -Path "$gitDirectory\vm-status-checks"){
-            $gitDirectoryExists = $true
+Describe "Important GitHub Repositories" {
+    Context "<_>" -ForEach 'vm-status-checks','tdm-demos','forkable-widget' {
+        It 'should be cloned to C:\git' {
+            $repoPath = Join-Path -Path $gitDirectory -ChildPath $_
+            Test-Path -Path $repoPath | Should -BeTrue
         }
-        $gitDirectoryExists | Should -BeTrue
-    }
 
-    It 'tdm-demos repository should be cloned to C:\git' {
-        $gitDirectoryExists = $false
-        If (Test-Path -Path "$gitDirectory\tdm-demos\.git"){
-            $gitDirectoryExists = $true
+        It "should be the latest version" {
+            $latestCommitHash = Get-LatestGitHubCommitHash -GitHubRepository "alex-yates-redgate/$_" -Branch 'main' #gets latest github commit hash
+            $currentCommitHash = (git -C "$gitDirectory\$_" log -1).Split() | Where-Object { $_.Length -eq 40 } #gets local commit hash
+            $latestCommitHash | Should -BeExactly $currentCommitHash
         }
-        $gitDirectoryExists | Should -BeTrue
-    }
-
-    It 'forkable-widget repository should be cloned to C:\git' {
-        $gitDirectoryExists = $false
-        If (Test-Path -Path "$gitDirectory\forkable-widget\.git"){
-            $gitDirectoryExists = $true
-        }
-        $gitDirectoryExists | Should -BeTrue
     }
 }
