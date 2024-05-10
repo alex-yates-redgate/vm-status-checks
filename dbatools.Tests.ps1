a4bf5545c9fb569da9acf9a589aebf74754e817a
alex-yates-redgate <alex.yates@red-gate.com>
2024-05-10 16:44:30 +0100

    Adding tests to validate the dbatools config

---
+++ dbatools.Tests.ps1
@@ -0,0 +1,12 @@
+BeforeAll {
+    import-module dbatools
+}
+
+Describe 'Testing dbatools config' {
+    It 'dbatools should trust the SQL Server certificate' {
+        (Get-DbatoolsConfig -FullName sql.connection.trustcert).Value | Should -BeTrue
+    }
+    It 'dbatools should not encypt the connection to SQL Server' {
+        (Get-DbatoolsConfig -FullName sql.connection.encrypt).Value | Should -BeFalse
+    }
+}
