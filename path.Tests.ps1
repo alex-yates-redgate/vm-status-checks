3084598461f08e0d366eae739ded5cf176126601
RyanSpr <162704784+RyanSpr@users.noreply.github.com>
2024-05-10 15:24:09 +0100

    Update path.Tests.ps1

--- path.Tests.ps1
+++ path.Tests.ps1
@@ -16,58 +16,11 @@ BeforeAll {
 }
 
 Describe 'The following tools should be accessible via %PATH% from the command line' {
-    It 'flyway' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "flyway"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'flyway-dev' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "flyway-dev"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'rgcompare' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "rgcompare"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'sqlcompare' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "sqlcompare"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'sqldatacompare' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "sqldatacompare"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'sqldatagenerator' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "sqldatagenerator"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'rgclone' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "rgclone"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'subsetter' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "subsetter"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'anonymize' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "anonymize"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'datagenerator' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "datagenerator"
-        $runsSuccessfully | Should -BeTrue
-    }
-
-    It 'DataMaskerCmdLine' {
-        $runsSuccessfully = Test-CommandRunsSuccessfully -command "DataMaskerCmdLine"
-        $runsSuccessfully | Should -BeTrue
-    }
+    It "<_> is in %PATH%" -ForEach (
+        'flyway', 'flyway-dev', 'rgcompare', 
+        'sqlcompare', 'sqldatacompare', 'sqldatagenerator', 
+        'rgclone', 'subsetter', 'anonymize', 
+        'datagenerator', 'DataMaskerCmdLine') {
+        
+        Test-CommandRunsSuccessfully -command $_ | Should -BeTrue}
 }
