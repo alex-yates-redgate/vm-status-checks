e50effcfaa9f7151969eff8751581e8629327822
alex-yates-redgate <alex.yates@red-gate.com>
2024-06-04 15:04:05 +0100

    startup script tests

--- startup.Tests.ps1
+++ startup.Tests.ps1
@@ -1,5 +1,6 @@
 BeforeAll {
     $startupApps = Get-CimInstance Win32_StartupCommand
+    $startupTasks = Get-ScheduledTask
 }
 
 Describe 'The following apps should run on startup' {
@@ -7,3 +8,30 @@ Describe 'The following apps should run on startup' {
         'ZoomIt' | Should -BeIn $startupApps.Name
     }
 }
+
+Describe 'Scheduled tasks should exist for the following scripts' {
+    It 'GitPullAll' {
+        'GitPullAll' | Should -BeIn $startupTasks.TaskName
+    }
+    It 'StartRedgateClient' {
+        'StartRedgateClient' | Should -BeIn $startupTasks.TaskName
+    }
+    It 'UpdateRgClone' {
+        'UpdateRgClone' | Should -BeIn $startupTasks.TaskName
+    }
+}
+
+Describe 'Scheduled tasks should be enabled' {
+    It 'GitPullAll' {
+        (Get-ScheduledTask  -TaskName "GitPullAll").Triggers[0].Enabled | Should -BeLike "True"
+    }
+    It 'StartRedgateClient' {
+        (Get-ScheduledTask  -TaskName "StartRedgateClient").Triggers[0].Enabled | Should -BeLike "True"
+    }
+    It 'UpdateRgClone' {
+        (Get-ScheduledTask  -TaskName "UpdateRgClone").Triggers[0].Enabled | Should -BeLike "True"
+    }
+}
+
+
+
