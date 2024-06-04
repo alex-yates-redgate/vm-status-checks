dd70b6657fb5abf12ab6501453fab3a03899808c
alex-yates-redgate <alex.yates@red-gate.com>
2024-06-04 15:28:04 +0100

    need to ensure the startup scripts repo is up to date

--- git.Tests.ps1
+++ git.Tests.ps1
@@ -22,7 +22,7 @@ Describe 'Checking the following directory exists' {
 }
 
 Describe "Important GitHub Repositories" {
-    Context "<_>" -ForEach 'vm-status-checks','tdm-demos','forkable-widget' {
+    Context "<_>" -ForEach 'vm-status-checks','tdm-demos','forkable-widget','vm-startup-scripts' {
         It 'should be cloned to C:\git' {
             $repoPath = Join-Path -Path $gitDirectory -ChildPath $_
             Test-Path -Path $repoPath | Should -BeTrue
