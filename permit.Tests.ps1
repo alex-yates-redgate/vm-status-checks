606883e2f020cc827ea6cfbd2ce38037102befde
alex-yates-redgate <alex.yates@red-gate.com>
2024-06-05 15:56:49 +0100

    Permit tests

---
+++ permit.Tests.ps1
@@ -0,0 +1,13 @@
+BeforeAll {
+    $permitPath = "C:\Program Files\Red Gate\Permits\permit.dat"
+}
+
+Describe 'Testing permit setup' {
+    It 'Permit file environment variable should exist correctly' {
+        $env:REDGATE_LICENSING_PERMIT_PATH | Should -BeLike $permitPath
+    }
+
+    It 'Permit file should exist'{
+        Test-Path $permitPath | Should -BeTrue 
+    }
+}
