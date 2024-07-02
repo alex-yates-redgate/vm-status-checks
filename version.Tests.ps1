6642ab2040fa408f10143200692062186b7d4cf9
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2024-07-02 11:54:58 +0100

    Now tests whether anonymize and subsetter are up to date

--- version.Tests.ps1
+++ version.Tests.ps1
@@ -1,3 +1,28 @@
+BeforeAll {
+    function Find-LatestVersion {
+        param (
+            [Parameter(Position=0,mandatory=$true)]$xml
+        )
+        # Parsing the XML to find all the available versions
+        $versions = @()
+        ($xml -Split "<Key>") | ForEach {
+            $versions += ((($_ -split ".zip")[0] -split "_")[1]) 
+        }
+    
+        # Remove duplicates
+        $uniqueVersions = $versions | Select-Object -Unique
+    
+        # Sort versions, based on [System.Version]. More info: https://learn.microsoft.com/en-us/dotnet/api/system.version.parse?view=net-8.0
+        $sortedVersions = $uniqueVersions | Sort-Object {
+            [System.Version]::Parse($_)
+        } -Descending
+    
+        # Return the biggest version
+        return $sortedVersions[0]
+    }
+}
+
+
 Describe 'Checking the following tools are up to date' {
     It 'Flyway' {
         $flywayVersion = flyway version
@@ -21,25 +46,17 @@ Describe 'Checking the following tools are up to date' {
         $upToDate | Should -BeTrue
     }
 
-    It 'anonymize timebomb' {
-        $anonymizeOutput = anonymize
-        If ($anonymizeOutput -like "*This version of the Anonymization tool has expired*"){
-            $upToDate = $false
-        }
-        else {
-            $upToDate = $true
-        }
-        $upToDate | Should -BeTrue
+    It 'anonymize' {
+        $anonymizeVersionsXml = (Invoke-WebRequest "https://redgate-download.s3.eu-west-1.amazonaws.com/?delimiter=/&prefix=EAP/AnonymizeWin64/").Content
+        $latestsAnonymize = Find-LatestVersion $anonymizeVersionsXml
+        $installedAnonymize = anonymize --version
+        $installedAnonymize | Should -BeLike  "*$latestsAnonymize*"
     }
 
-    It 'subset timebomb' {
-        $subsetOutput = subsetter
-        If ($subsetOutput -like "*This version of the Subsetter tool has expired*"){
-            $upToDate = $false
-        }
-        else {
-            $upToDate = $true
-        }
-        $upToDate | Should -BeTrue
+    It 'subsetter' {
+        $subsetterVersionsXml = (Invoke-WebRequest "https://redgate-download.s3.eu-west-1.amazonaws.com/?delimiter=/&prefix=EAP/SubsetterWin64/").Content
+        $latestsSubsetter = Find-LatestVersion $subsetterVersionsXml
+        $installedSubsetter = subsetter --version
+        $installedSubsetter | Should -BeLike  "*$latestsSubsetter*"
     }
 }
