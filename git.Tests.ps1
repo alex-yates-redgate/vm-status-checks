de34bfdffd425c6857616b9ca3215c817fb26473
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2024-10-07 14:03:44 +0100

    Failing test to clone flyway templates to C:\git

--- git.Tests.ps1
+++ git.Tests.ps1
@@ -71,6 +71,8 @@ Describe "Important GitHub Repositories" {
         @{ Repo = "Flyway-AutoPilot-FastTrack-AzureDevOps"; Tag = 'SalesDemo' }
         @{ Repo = "Flyway-AutoPilot-FastTrack-GitHub"; Tag = 'SalesDemo' }
         @{ Repo = "Flyway-AutoPilot-FastTrack"; Tag = 'CustomerVM' }
+        @{ Repo = "Flyway-Templates-GitHub"; Tag = 'SalesDemo' }
+        @{ Repo = "Flyway-Tempaltes-AzureDevOps"; Tag = 'SalesDemo' }
     ) {
         It -Tag $tag "$repo should be cloned to C:\git" {
             $repoPath = Join-Path -Path $gitDirectory -ChildPath $repo
